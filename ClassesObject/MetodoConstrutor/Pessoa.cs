fcf1998e13d04d25f4ac9a6b441b84213ea792c1
fabiovalinhos <fabiovalinhos@gmail.com>
2024-07-02 15:34:59 -0300

    Overloading no construtor

--- ClassesObject/MetodoConstrutor/Pessoa.cs
+++ ClassesObject/MetodoConstrutor/Pessoa.cs
@@ -20,5 +20,28 @@ namespace MetodoConstrutor
             anoNascimento = 0;
             idade = 0;
         }
+
+        public Pessoa(string nome,
+        string sobrenome,
+        int anoNascimento)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = anoNascimento;
+            this.idade = Idade();
+        }
+
+        public Pessoa(string nome,
+        string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = 2002;
+            this.idade = Idade();
+        }
+
+        private int Idade(){
+            return 2024 - this.anoNascimento;
+        }
     }
 }
--- ClassesObject/MetodoConstrutor/Program.cs
+++ ClassesObject/MetodoConstrutor/Program.cs
@@ -7,5 +7,20 @@ Console.WriteLine(p1.sobrenome);
 Console.WriteLine(p1.anoNascimento);
 Console.WriteLine(p1.idade);
 
+Console.WriteLine();
+
+Pessoa p2 = new("Bruce", "Fletcher", 1973);
+Console.WriteLine(p2.nome);
+Console.WriteLine(p2.sobrenome);
+Console.WriteLine(p2.anoNascimento);
+Console.WriteLine(p2.idade);
+
+Console.WriteLine();
+
+Pessoa p3 = new("Fábio", "Valinhos");
+Console.WriteLine(p3.nome);
+Console.WriteLine(p3.sobrenome);
+Console.WriteLine(p3.anoNascimento);
+Console.WriteLine(p3.idade);
 
 Console.ReadKey();
