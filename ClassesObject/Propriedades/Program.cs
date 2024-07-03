5a83cd9e2b0d3eebdb60263d1f512f8bb4388225
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-07-03 10:30:31 -0300

    Propriedades da classe

---
+++ ClassesObject/Propriedades/Program.cs
@@ -0,0 +1,11 @@
+﻿using Propriedades;
+
+Teste t = new();
+
+t.Nome = "Bruce";
+t.Idade = 51;
+
+Console.WriteLine($"{t.Nome} {t.Sobrenome}");
+Console.WriteLine($"{t.Idade}");
+
+Console.ReadKey();
---
+++ ClassesObject/Propriedades/Teste.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+    public class Teste
+    {
+        private string _nome;
+
+        public string Sobrenome { get; } = "Fletcher";
+
+        private int _idade;
+
+        //props
+        public string Nome { get { return _nome; } set { _nome = value; } }
+
+        public int Idade
+        {
+            get { return _idade; }
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Idade não pode ser inferior a 18 anos");
+                }
+                else { _idade = value; }
+            }
+        }
+
+
+
+        //////
+        public void Apresentar()
+        {
+            if (!string.IsNullOrEmpty(_nome))
+            {
+                Console.WriteLine($"Bem vindo {_nome}");
+            }
+        }
+    }
+}
