b44a1c592e404cfeb4f3850ec41a6b9e516c873a
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-07-03 11:19:27 -0300

    Criado um encapsulamento de uma projeto

---
+++ ClassesObject/Encapsular/Program.cs
@@ -0,0 +1,17 @@
+﻿using Encapsular;
+
+Conta c = new();
+
+c.Cliente = "Bruce";
+
+
+double valor = 100;
+c.Depositar(valor);
+
+double valor2 = 50;
+c.Sacar(valor2);
+
+Console.WriteLine($"Cliente: {c.Cliente}");
+Console.WriteLine($"Saldo: {c.Saldo}");
+
+Console.ReadKey();
