00605a01b518596c964e9d80180bc963be0907bc
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-07-04 15:02:12 -0300

    Modificadores de acesso public private protected internal

---
+++ ClassesObject/Modificadores/Classes.cs
@@ -0,0 +1,40 @@
+﻿namespace Modificadores
+{
+    class Teste
+    {
+        private string nome;
+        
+        public string sobrenome;
+
+        private void Metodo1()
+        {
+
+        }
+
+        public void Executar()
+        {
+
+        }
+    }
+
+    class Humano
+    {
+        protected string nome;
+
+        private string sobrenome;
+
+        // se for criado uma dll desta classe este internal
+        // exclusivamente dela
+        //disponível ao mesmo assembly, desta aplicação
+        internal int idade;
+    }
+
+    class Homem : Humano
+    {
+        public void Metodo()
+        {
+            nome = string.Empty;
+            idade = 0;
+        }
+    }
+}
---
+++ ClassesObject/Modificadores/Program.cs
@@ -0,0 +1,11 @@
+﻿using Modificadores;
+
+Teste t = new();
+
+//t.nome = "";
+t.sobrenome = "";
+//t.Metodo1();
+t.Executar();
+
+Homem h = new();
+h.idade = 51;
