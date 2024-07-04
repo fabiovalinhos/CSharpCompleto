d239971a3c52539ad4aa69b723c65d12bb001fb1
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-07-04 11:28:54 -0300

    Membros sealed

---
+++ ClassesObject/Heranca/Classes.cs
@@ -0,0 +1,42 @@
+﻿namespace Heranca
+{
+     class Humano
+    {
+        public virtual void Olhos()
+        {
+            Console.WriteLine("Humano.Olhos");
+        }
+
+        public virtual void Cabelos()
+        {
+            Console.WriteLine("Humano.Cabelos");
+        }
+    }
+
+    class Pessoa : Humano 
+    {
+        public sealed override void Olhos()
+        {
+            Console.WriteLine("Pessoa.Olhos");
+        }
+
+        public override void Cabelos()
+        {
+            Console.WriteLine("Pessoa.Cabelos");
+        }
+    }
+
+    class Homem : Pessoa
+    {
+        // Está sealed no Pessoa
+        //public override void Olhos()
+        //{
+        //    Console.WriteLine("Homem.Olhos");
+        //}
+
+        public override void Cabelos()
+        {
+            Console.WriteLine("Homem.Cabelos");
+        }
+    }
+}
--- ClassesObject/Heranca/Program.cs
+++ ClassesObject/Heranca/Program.cs
@@ -1,13 +1,30 @@
 ﻿using Heranca;
 
-Carro c = new Carro();
-Bicicleta b = new();
+#region Parte 1 herança e sealed
+//Carro c = new Carro();
+//Bicicleta b = new();
 
-c.Acelerar();
-b.Acelerar();
+//c.Acelerar();
+//b.Acelerar();
 
-c.Parar();
-b.Parar();
+//c.Parar();
+//b.Parar();
+#endregion
 
+Humano hu = new();
+Pessoa p = new();
+Homem ho = new();
+
+Console.WriteLine("\nHumano");
+hu.Olhos();
+hu.Cabelos();
+
+Console.WriteLine("\nPessoa");
+p.Olhos();
+p.Cabelos();
+
+Console.WriteLine("\nHomem");
+ho.Olhos();
+ho.Cabelos();
 
 Console.ReadKey();
