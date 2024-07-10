f1a98418c651b64b11c6298888a848ab5e7283de
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2024-07-10 14:52:19 -0300

    Dictionary

---
+++ Colecoes/Dictionary/ClasseDictionary.cs
@@ -0,0 +1,28 @@
+﻿namespace Dictionary
+{
+    public class ClasseDictionary
+    {
+        public Dictionary<int, string> alunos = new()
+        {
+            { 150,"Danny"},
+            { 200, "Arthur"},
+            { 80, "Danny"}
+        };
+
+        public void Acionando()
+        {
+            alunos.Add(100, "Gabriel");
+
+            //var res = alunos.Count();
+            //var res = alunos.Remove(100);
+            //var res = alunos.Clear();
+            var res = alunos.ContainsKey(70);
+            //var res = alunos.ContainsValue("Jonas");
+            //var res = alunos.First().Value;
+            //var res = alunos.Last();
+
+            Console.WriteLine($"Teste de saida {res}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Colecoes/Dictionary/Program.cs
@@ -0,0 +1,10 @@
+﻿using Dictionary;
+
+ClasseDictionary classeDictionary = new();
+
+classeDictionary.Acionando();
+
+foreach (KeyValuePair<int, string> item in classeDictionary.alunos)
+{
+    Console.WriteLine($"Chave {item.Key} com valor {item.Value}");
+}
